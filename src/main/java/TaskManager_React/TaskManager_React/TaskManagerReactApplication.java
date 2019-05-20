3c945427e26214cda2581659a9b7dd0b0d10267c
d.burmistrov <6y6yka1997@gmail.com>
2019-05-20 11:34:36 +0300

    started project

---
+++ src/main/java/TaskManager_React/TaskManager_React/TaskManagerReactApplication.java
@@ -0,0 +1,13 @@
+package TaskManager_React.TaskManager_React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerReactApplication.class, args);
+	}
+
+}
